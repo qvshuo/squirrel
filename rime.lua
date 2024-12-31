6c2549210864c407be91fea9d81c38d48ecd903c
qvshuo <qvshuo@foxmail.com>
2025-01-01 02:39:41 +0800

    rm librime-cloud

--- rime.lua
+++ rime.lua
@@ -1,7 +1,3 @@
 -- 降低部分英语单词在候选项的位置，可在方案中配置要降低的模式和单词
 reduce_english_filter = require("reduce_english_filter")
 
--- 百度云拼音，Control+t 为触发键
-local cloud_pinyin = require("trigger")("Control+t", require("baidu"))
-cloud_pinyin_translator = cloud_pinyin.translator
-cloud_pinyin_processor = cloud_pinyin.processor
