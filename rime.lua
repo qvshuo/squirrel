98cce87a4a7bd860aa5dfe00ecc8d7716d3cc91f
qvshuo <qvshuo@foxmail.com>
2024-12-31 17:12:31 +0800

    remove google cloud input method

--- rime.lua
+++ rime.lua
@@ -1,12 +1,7 @@
 -- 降低部分英语单词在候选项的位置，可在方案中配置要降低的模式和单词
 reduce_english_filter = require("reduce_english_filter")
 
---- 云拼音，Control+t 为云输入触发键
---- 使用方法：
---- 将 "lua_translator@cloud_pinyin_translator" 和 "lua_processor@cloud_pinyin_processor"
---- 分别加到输入方案的 engine/translators 和 engine/processors 中
-local cloud_pinyin_provider = require("baidu")
--- local cloud_pinyin_provider = require("google")
-local cloud_pinyin = require("trigger")("Control+t", cloud_pinyin_provider)
+-- 百度云拼音，Control+t 为触发键
+local cloud_pinyin = require("trigger")("Control+t", require("baidu"))
 cloud_pinyin_translator = cloud_pinyin.translator
 cloud_pinyin_processor = cloud_pinyin.processor
